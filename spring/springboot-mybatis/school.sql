0f33364d5bed7a6927328ab0cc09447e80ced4b5
Bruce Lee <ltz121320@163.com>
2017-05-31 23:02:07 +0800

    update db file

--- spring/springboot-mybatis/school.sql
+++ spring/springboot-mybatis/school.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost - mysql
+Source Server         : gcrsdb
 Source Server Version : 50718
 Source Host           : localhost:3306
 Source Database       : school
@@ -10,10 +10,49 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2017-05-27 14:13:18
+Date: 2017-05-31 23:00:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for blog
+-- ----------------------------
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of blog
+-- ----------------------------
+INSERT INTO `blog` VALUES ('1', 'Java', 'Java空指针', '李杰');
+INSERT INTO `blog` VALUES ('2', 'C++', 'C++智能指针', '王超文');
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) DEFAULT NULL,
+  `comment_date` datetime DEFAULT NULL,
+  `blog_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `blog` (`blog_id`),
+  CONSTRAINT `blog` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES ('1', '写的不错', '2017-05-30 23:12:27', '1');
+INSERT INTO `comment` VALUES ('2', 'good', '2017-05-30 23:12:45', '2');
+INSERT INTO `comment` VALUES ('3', '垃圾', '2017-05-30 23:22:56', '1');
 
 -- ----------------------------
 -- Table structure for student
@@ -29,14 +68,15 @@ CREATE TABLE `student` (
   PRIMARY KEY (`id`),
   KEY `teacher` (`teacher_id`),
   CONSTRAINT `teacher` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('1', '梁英飞', '男', '计算机系统结构', '2011', '2');
-INSERT INTO `student` VALUES ('2', '付少红', '女', '计算机软件与理论', '2011', '1');
+INSERT INTO `student` VALUES ('1', '张三', '男', '生物科学', '2010', '1');
+INSERT INTO `student` VALUES ('2', '梁英飞', '男', '计算机系统结构', '2011', '1');
 INSERT INTO `student` VALUES ('3', '刘丹', '女', '计算机软件与理论', '2011', '1');
+INSERT INTO `student` VALUES ('4', '王超文', '男', '计算机技术', '2011', '1');
 
 -- ----------------------------
 -- Table structure for teacher
@@ -44,15 +84,14 @@ INSERT INTO `student` VALUES ('3', '刘丹', '女', '计算机软件与理论', 
 DROP TABLE IF EXISTS `teacher`;
 CREATE TABLE `teacher` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `gender` char(10) DEFAULT NULL,
   `research_area` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
 INSERT INTO `teacher` VALUES ('1', '王念滨', '男', '数据库与数据挖掘', '教授');
-INSERT INTO `teacher` VALUES ('2', '张健沛', '男', '数据库', '教授');
