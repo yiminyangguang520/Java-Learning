01c988851ec50e26a2ce55c1b9ebfd073bede8b3
litz-a <litz-a@glodon.com>
2018-07-04 20:28:33 +0800

    add spring boot redis cache sample

---
+++ spring/spring-boot/spring-boot-redis-cache-sample/src/main/java/com/xncoding/trans/dao/repository/UserMapper.java
@@ -0,0 +1,11 @@
+package com.xncoding.trans.dao.repository;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.xncoding.trans.dao.entity.User;
+
+/**
+ * @author litz-a
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
