e8f05fa4b296051d1cf8283c1b8f89e769875e6a
litz-a <litz-a@glodon.com>
2018-07-16 09:52:13 +0800

    add spring boot rabbitmq dead letter sample

---
+++ spring/spring-boot/spring-boot-rabbitmq-dead-letter-sample/src/main/java/com/lee/rabbitmq/DeadLetterExampleService.java
@@ -0,0 +1,18 @@
+package com.lee.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author litz-a
+ */
+@SpringBootApplication
+@EnableScheduling
+public class DeadLetterExampleService {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(DeadLetterExampleService.class, args);
+  }
+
+}
---
+++ spring/spring-boot/spring-boot-rabbitmq-dead-letter-sample/src/main/java/com/lee/rabbitmq/domain/ExampleObject.java
@@ -0,0 +1,30 @@
+package com.lee.rabbitmq.domain;
+
+import java.util.Date;
+
+/**
+ * @author litz-a
+ */
+public class ExampleObject {
+
+  private Date date = new Date();
+
+  public ExampleObject() {
+  }
+
+  @Override
+  public String toString() {
+    return "ExampleObject{" +
+        "date= " + date +
+        '}';
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+}
